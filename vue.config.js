45c9a4d24e3f9e4e599335a526fc91a7812ba15f
ELFI-303 <60342473+ELFI-303@users.noreply.github.com>
2023-12-17 21:38:12 +0100

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   devServer: {
-    proxy: 'https://bbi3cqg9ld.execute-api.eu-west-1.amazonaws.com/'
+    proxy: 'https://bbi3cqg9ld.execute-api.eu-west-1.amazonaws.com/',
+    https: true
   }
 };
