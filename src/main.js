8912eede1f29ee82189cb51ca07a3b627c58b8bf
ELFI-303 <60342473+ELFI-303@users.noreply.github.com>
2025-03-16 15:06:22 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from '@/router/router.js';
 import store from '@/store/store.js';
 
 //axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'https://bbi3cqg9ld.execute-api.eu-west-1.amazonaws.com/test_hellolambda';
+axios.defaults.baseURL = '${lambdaUrl}';
 
 const app = createApp(App);
 app.use(router);
